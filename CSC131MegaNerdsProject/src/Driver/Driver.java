ee49d679ef8dd3020f6d40240dc7a62adc4967e0
Ethan <Ethan@LAPTOP-3M2UU81B>
2022-04-22 22:56:39 -0700

    User can create their own notes, added comments to NoteBoard

--- CSC131MegaNerdsProject/src/Driver/Driver.java
+++ CSC131MegaNerdsProject/src/Driver/Driver.java
@@ -9,11 +9,6 @@ public class Driver {
 
 	public static void main(String[] args) {
 		NotesList test = new NotesList();
-		test.addNote("title", "text", LocalDateTime.now());
-		test.addNote("title", "text", LocalDateTime.now());
-		test.addNote("title", "text", LocalDateTime.now());
-		Note ex = test.get(0);
-		System.out.println(ex.getTitle() + ex.getText() + ex.getTime() + ex.getCreation() + ex.getModification());
 		NoteWindow testWindow = new NoteWindow(test);
 		testWindow.setVisible(true);
 	}
--- CSC131MegaNerdsProject/src/NoteFrontEnd/NoteBoard.java
+++ CSC131MegaNerdsProject/src/NoteFrontEnd/NoteBoard.java
@@ -1,14 +1,21 @@
 package NoteFrontEnd;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 import javax.swing.*;
 import NoteBackEnd.*;
 
 public class NoteBoard extends JPanel{
 	
+	//contains a reference to the NotesList list
 	private NotesList data;
+	//used for storing the two sets of button
+	private ArrayList<NoteButton> noteBtn = new ArrayList<NoteButton>();
+	private ArrayList<Button> delete = new ArrayList<Button>();
 	
+	//takes a reference to NotesList as parameter, constructor
 	public NoteBoard(NotesList data) {
 		super();
 		this.data = data;
@@ -16,15 +23,86 @@ public class NoteBoard extends JPanel{
 		this.setVisible(true);
 	}
 	
+	//returns and accepts nothing, used for initial setup of the frame
 	public void setupLayout() {
-		GridLayout grid = new GridLayout();
-		grid.setColumns(1);
-		grid.setRows(data.size());
+		//initializing and setting up a grid layout manager;
+		GridLayout grid = new GridLayout(0, 2);
+
+		//sets frame to use the grid layout manager previously created
 		this.setLayout(grid);
-		ArrayList<NoteButton> noteBtn = new ArrayList<NoteButton>();
-		for (int i = 0; i < data.size(); i++) {
-			noteBtn.add(new NoteButton(data.get(i)));
-			this.add(noteBtn.get(i));
+		
+		//Setting up elements
+		Label titleLabel = new Label("Notes");
+		titleLabel.setBackground(Color.yellow);
+		Button newNote = new Button("Create New Note");
+		newNote.setBackground(Color.green);
+		
+		//Adds anonymous method for event handling to create a new note
+		newNote.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JPanel panel = new JPanel();
+				panel.setLayout(new GridLayout(1,2));
+				
+				//dialog box is too small for the panel, dialog box needs to be resized
+				panel.setSize(1000,1000);
+				
+				//setting up elements in a frame to pass to dialog box
+				JLabel titleLabel = new JLabel("Title:");
+				JLabel textLabel = new JLabel("Text:");
+				JFormattedTextField titleBox = new JFormattedTextField("");
+				JFormattedTextField textBox = new JFormattedTextField("");
+				panel.add(titleLabel);
+				panel.add(titleBox);
+				panel.add(textLabel);
+				panel.add(textBox);
+				
+				//if the user presses yes on the dialog box, takes the data from the box and creates a new Note
+				int noteEdit = JOptionPane.showConfirmDialog(null, panel);
+				if (noteEdit == JOptionPane.YES_OPTION) {
+					data.addNote(titleBox.getText(), titleBox.getText(), null);
+					refresh();
+				}
+			}
+		});
+		
+		this.add(titleLabel);
+		this.add(newNote);
+		refresh();
+	}
+	
+	//returns and accepts void, used for refreshing all the components in the frame
+	public void refresh() {
+		//when there are more notes than pairs of button, sets the label of each button to the corresponding title and creates a new pair of button for notes without buttons
+		if (data.size() > noteBtn.size()) {
+			for (int i = 0; i < data.size(); i++) {
+				if (i < noteBtn.size()) {
+					noteBtn.get(i).setLabel(data.get(i).getTitle());
+				} else {
+					noteBtn.add(new NoteButton(data.get(i)));
+					delete.add(new Button("Delete"));
+					this.add(noteBtn.get(i));
+					this.add(delete.get(i));
+					this.revalidate();
+				}
+			}
+		} else {
+		//when there are more pair of buttons than notes, sets the title to the corresponding buttons and removes the extra
+			ArrayList<Integer> nums = new ArrayList<Integer>();
+			for (int i = 0; i < noteBtn.size(); i++) {
+				if (i < noteBtn.size()) {
+					noteBtn.get(i).setLabel(data.get(i).getTitle());
+				} else {
+					//adds the index of items that need to removed, the items are removed in reverse order
+					nums.add(i);
+				}
+			}
+			//removing the buttons from the frame and lists
+			for (int i = nums.size() - 1; i > 0; i--) {
+				this.remove(noteBtn.get(i));
+				this.remove(delete.get(i));
+				noteBtn.remove(i);
+				delete.remove(i);
+			}
 		}
 	}
 }
