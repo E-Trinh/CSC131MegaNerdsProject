9bb1159095f3485ed566cc7a300b1d797154f911
MattDomi <mattelzo@yahoo.com>
2022-04-27 16:18:16 -0700

    Implemented bubble sort to OldToNewSort

--- CSC131MegaNerdsProject/src/NoteBackEnd/OldToNewSort.java
+++ CSC131MegaNerdsProject/src/NoteBackEnd/OldToNewSort.java
@@ -1,10 +1,34 @@
 package NoteBackEnd;
 
+
 import java.util.ArrayList;
+
 
 public class OldToNewSort implements NoteSort{
 	@Override
-	public ArrayList<Note> sort(ArrayList<Note> notes) {
-		return null;
+	public ArrayList<Note> sort(ArrayList<Note> notes){
+		return compare(notes);																					// returned sorted arraylist of notes
 	}
+	
+public ArrayList<Note> compare(ArrayList<Note> toSort) {
+	ArrayList<Note> sorted = new ArrayList<Note>();
+	Note noteTemp;
+	
+	
+		if (sorted.size() > 1)	{																				// check if passed arraylist is larger than 1
+		for (int i = 0; i < toSort.size(); i++) {																// outer loop of bubble sort
+			for( int j = 0; j < toSort.size() - i - 1; j++) {													// inner loop of bubble sort
+				if (toSort.get(i).getModification().isBefore(toSort.get(i+1).getModification()))				// if last modification date of note1 is less than last mod date of note2
+				{
+					noteTemp = toSort.get(i);												
+					sorted.set(i, toSort.get(i+1));
+					sorted.set(i+1, noteTemp);
+				}
+			}
+		
+
+		}
+}
+		return sorted;																							// return sorted arraylist
+}
 }
